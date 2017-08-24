fa53f52ebd341d24debb27084d36b50739576828
Keef Aragon <keef.aragon@konscious.net>
2017-08-23 18:49:12 -0700

    Forcing cmake generator and adding code coverage

--- Conan.cmake
+++ Conan.cmake
@@ -1,16 +1,12 @@
-macro(parse_arguments)
-    set(options BASIC_SETUP CMAKE_TARGETS)
-    set(oneValueArgs BUILD CONANFILE)
-    set(multiValueArgs REQUIRES OPTIONS IMPORTS CONAN_COMMAND)
-    cmake_parse_arguments(ARGUMENTS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
-endmacro()
-
 function(_conan_install conanfile)
     # Calls "conan install"
     # Argument BUILD is equivalant to --build={missing, PkgName,...}
     # Argument CONAN_COMMAND, to specify the conan path, e.g. in case of running from source
     # cmake does not identify conan as command, even if it is +x and it is in the path
-    parse_arguments(${ARGN})
+    set(options "")
+    set(oneValueArgs BUILD CONANFILE)
+    set(multiValueArgs REQUIRES OPTIONS IMPORTS CONAN_COMMAND)
+    cmake_parse_arguments(ARGUMENTS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
 
     if(ARGUMENTS_BUILD)
         set(CONAN_BUILD_POLICY "--build=${ARGUMENTS_BUILD}")
@@ -27,7 +23,7 @@ function(_conan_install conanfile)
         set(conanfile "${CMAKE_CURRENT_SOURCE_DIR}/${conanfile}")
     endif()
 
-    set(conan_args install -f "${conanfile}" ${CONAN_BUILD_POLICY})
+    set(conan_args install -g cmake -f "${conanfile}" ${CONAN_BUILD_POLICY})
 
     string (REPLACE ";" " " _conan_args "${conan_args}")
     set(conan_cmake_file "${CMAKE_CURRENT_BINARY_DIR}/conanbuildinfo.cmake")
@@ -35,7 +31,7 @@ function(_conan_install conanfile)
     if("${conanfile}" IS_NEWER_THAN "${conan_cmake_file}")
         message(STATUS "Conan executing: ${conan_command} ${_conan_args}")
 
-        execute_process(COMMAND "${conan_command}" ${conan_args}
+        execute_process(COMMAND ${conan_command} ${conan_args}
             RESULT_VARIABLE return_code
             WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
 
---
+++ Coverage.cmake
@@ -0,0 +1,46 @@
+
+function(add_coverage_run _target_name _for_target)
+    set(options "")
+    set(oneValueArgs "FORMAT")
+    set(multiValueArgs "ARGS")
+    cmake_parse_arguments(COVERAGE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    find_program(LCOV_EXE lcov)
+    find_program(GENHTML_EXE genhtml)
+    find_program(XDG_OPEN_EXE xdg-open)
+
+    if (NOT LCOV_EXE)
+        message(ERROR "lcov is required for code coverage")
+    endif()
+
+    if (NOT GENHTML_EXE)
+        message(ERROR "genhtml is required for code coverage")
+    endif()
+
+    if (NOT XDG_OPEN_EXE)
+        message(ERROR "xdg-open is required for code coverage")
+    endif()
+
+    get_target_property(TARGET_OUTPUT_DIR "${_for_target}" RUNTIME_OUTPUT_DIRECTORY_${CMAKE_BUILD_TYPE})
+    if (NOT TARGET_OUTPUT_DIR)
+        get_target_property(TARGET_OUTPUT_DIR "${_for_target}" RUNTIME_OUTPUT_DIRECTORY)
+    endif()
+    get_target_property(TARGET_SUFFIX "${_for_target}" SUFFIX)
+
+    # just get all the project sources for the extract
+    file(GLOB_RECURSE _all_sources "${CMAKE_SOURCE_DIR}/*")
+    set(_object_dir "${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/${_for_target}.dir")
+    add_custom_target("${_target_name}"
+            COMMAND ${LCOV_EXE} --zerocounters --directory "${_object_dir}"
+            COMMAND ${LCOV_EXE} --capture --initial --directory "${_object_dir}" --output-file "${_target_name}_coverage.run.info.all"
+            COMMAND "${TARGET_OUTPUT_DIR}/${_for_target}${SUFFIX}" ${COVERAGE_ARGS}
+            COMMAND ${LCOV_EXE} --capture --directory "${_object_dir}" --output-file "${_target_name}_coverage.run.info.all"
+            COMMAND ${LCOV_EXE} --directory "${_object_dir}" --output-file "${_target_name}_coverage.run.info" --extract "${_target_name}_coverage.run.info.all" ${_all_sources}
+            COMMAND ${GENHTML_EXE} -o "${CMAKE_CURRENT_BINARY_DIR}/${_target_name}" "${_target_name}_coverage.run.info"
+            COMMAND ${CMAKE_COMMAND} -E remove "${_target_name}_coverage.run.info"
+            COMMAND ${XDG_OPEN_EXE} "${CMAKE_CURRENT_BINARY_DIR}/${_target_name}/index.html"
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+            DEPENDS "${_for_target}"
+            COMMENT "Running and processing code coverage"
+            )
+endfunction()
